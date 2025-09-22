fd5c6d417c315ad2f47581f9fb4106e2d6f9ec36
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-22 12:41:46 +0200

    Tilføjet TestPrice og TestVichelType i TestBridge projektet.
    Tilføjet dokumentation i Car classen

--- Bridge/Car.cs
+++ Bridge/Car.cs
@@ -6,17 +6,30 @@ using System.Threading.Tasks;
 
 namespace Bridge
 {
-    internal class Car
+    public class Car
     {
+        /// <summary>
+        /// Property that contains the Licensplate as a string
+        /// </summary>
         public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Property that contains the date the car passed as DateTime
+        /// </summary>
         public DateTime Date { get; set; }
 
-
+        /// <summary>
+        /// A method that returns a fixed price
+        /// </summary>
+        /// <returns>the double 230</returns>
         public double Price() 
         {
             return 230;
         }
-
+        /// <summary>
+        /// A method that returns the type of vechile
+        /// </summary>
+        /// <returns>Returns the string Car</returns>
         public string VehicleType() 
         {
             return "Car";
---
+++ TestBridge/TestCar.cs
@@ -0,0 +1,37 @@
+﻿using Bridge;
+
+namespace TestBridge
+{
+    [TestClass]
+    public sealed class TestCar
+    {
+        [TestMethod]
+        public void TestPrice()
+        {
+            //Arrange
+            Car c = new Car();
+
+            //Act
+            double result = c.Price();
+
+            //Assert
+            Assert.AreEqual(result, 230);
+        }
+
+        [TestMethod]
+        public void TestVehicleType()
+        {
+            //Arrange
+            Car c = new Car();
+
+            //Act
+            string result = c.VehicleType();
+
+            //Assert
+            Assert.AreEqual(result, "Car");
+        }
+
+
+
+    }
+}
