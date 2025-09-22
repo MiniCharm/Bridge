e26e70ae6e9aa1db235ea3e2616561aefce15746
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-22 12:43:58 +0200

    Tilføjet MC klasse med dokumentation

---
+++ Bridge/MC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class MC
+    {
+        /// <summary>
+        /// Property that contains the Licensplate as a string
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Property that contains the date the Mc passed as DateTime
+        /// </summary>
+        public DateTime Date { get; set; }
+
+
+        /// <summary>
+        /// A method that returns a fixed price
+        /// </summary>
+        /// <returns>the double 120</returns>
+        public double Price()
+        {
+            return 120;
+        }
+        /// <summary>
+        /// A method that returns the type of vechile
+        /// </summary>
+        /// <returns>Returns the string MC</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+
+    }
+}
