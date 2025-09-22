d9f67eac002302e9850ee29921f8c344333dcc38
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-22 12:08:52 +0200

    Implementet class Car

---
+++ Bridge/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    internal class Car
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public double Price() 
+        {
+            return 230;
+        }
+
+        public string VehicleType() 
+        {
+            return "Car";
+        }
+    }
+}
