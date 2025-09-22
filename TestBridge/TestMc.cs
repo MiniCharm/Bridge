af1344419d55c47bc759c0b5f619320dd8c29a91
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-22 12:48:43 +0200

    Test class TestMC added including test TestPriceMC and TestVehivleTypeMC

---
+++ TestBridge/TestMc.cs
@@ -0,0 +1,37 @@
+﻿using Bridge;
+
+namespace TestBridge
+{
+    [TestClass]
+    public sealed class TestMC
+    {
+        [TestMethod]
+        public void TestPriceMC()
+        {
+            //Arrange
+            MC mc = new MC();
+
+            //Act
+            double result = mc.Price();
+
+            //Assert
+            Assert.AreEqual(result, 120);
+        }
+
+        [TestMethod]
+        public void TestVehicleTypeMC()
+        {
+            //Arrange
+            MC mc = new MC();
+
+            //Act
+            string result = mc.VehicleType();
+
+            //Assert
+            Assert.AreEqual(result, "MC");
+        }
+
+
+
+    }
+}
