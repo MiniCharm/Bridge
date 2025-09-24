0684eaca401b5657e5e1962604a003ee513ac33e
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-24 13:21:19 +0200

    Tilføjet reposetory klasse til StorbealtTicketLibrary. Følgende er implementeret: AddTicket, GetAllickets og GetTickets from plate
    Tilføjet data i statisk liste samt test af reposetory klassen

---
+++ StoreBealtTicketLibrary/Reposetory.cs
@@ -0,0 +1,33 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBealtTicketLibrary
+{
+    public class Reposetory
+    {
+        private static List<Vehicle> _vehicles = new List<Vehicle> 
+        { 
+                new Car("2222222", new DateTime(2025, 07, 05), false), 
+                new Car("1111111", new DateTime(2025, 08, 05), true) 
+        };
+
+        public void AddTicket(Vehicle vehicle) 
+        {
+            _vehicles.Add(vehicle);
+        }
+
+        public List<Vehicle> GetAllTickets() 
+        {
+            return _vehicles;
+        }
+
+        public List<Vehicle> GetTicketsFromPlate(string licenseplate)
+        {
+            return _vehicles.Where(v => v.Licenseplate == licenseplate).ToList();
+        }
+    }
+}
---
+++ TestBridge/TestReposetoryStorbealt.cs
@@ -0,0 +1,62 @@
+﻿using Bridge;
+using StoreBealtTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBridge
+{
+    [TestClass]
+    public sealed class TestReposetoryStorbealt
+    {
+        [TestMethod]
+        public void GetAllTickets()
+        {
+            //Arrange
+            Reposetory storRepo = new Reposetory();
+            List<Vehicle> listOfV = storRepo.GetAllTickets();
+
+
+            //Assert
+            Assert.IsNotNull(listOfV);
+        }
+
+        [TestMethod]
+        public void TestAddTicket()
+        {
+            //Arrange
+            Reposetory storRepo = new Reposetory();
+            List<Vehicle> listOfV = storRepo.GetAllTickets();
+            Vehicle v = new Car("1111111", new DateTime(2025, 07, 05), false);
+
+            //act
+            int countBEfore = listOfV.Count();
+            storRepo.AddTicket(v);
+            int countAfter = listOfV.Count();
+
+            //Assert
+            Assert.AreEqual(countBEfore + 1, countAfter);
+        }
+
+        [TestMethod]
+        public void GetTicketsFromLicensplate()
+        {
+            //Arrange
+            Reposetory storRepo = new Reposetory();
+            List<Vehicle> listOfV = storRepo.GetAllTickets();
+            Vehicle v = new Car("5555555", new DateTime(2025, 07, 05), false);
+            Vehicle v1 = new Car("5555555", new DateTime(2025, 09, 03), true);
+
+            //act
+            storRepo.AddTicket(v);
+            storRepo.AddTicket(v1);
+            List<Vehicle> found= storRepo.GetTicketsFromPlate("5555555");
+
+            Assert.AreEqual(found.Count(), 2);
+
+        }
+
+    }
+}
