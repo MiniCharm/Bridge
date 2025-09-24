d21cb7d57b4c857c76cfcaec755519c068234c87
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-24 13:21:19 +0200

    Tilføjet reposetory klasse til StorbealtTicketLibrary. Følgende er implementeret: AddTicket, GetAllickets og GetTickets from plate

---
+++ StoreBealtTicketLibrary/Reposetory.cs
@@ -0,0 +1,29 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBealtTicketLibrary
+{
+    public class Reposetory
+    {
+        private static List<Vehicle> _vehicles = new List<Vehicle>();
+
+        public void AddTicket(Vehicle vehicle) 
+        {
+            _vehicles.Add(vehicle);
+        }
+
+        public List<Vehicle> GetAllTickets() 
+        {
+            return _vehicles;
+        }
+
+        public List<Vehicle> GetTicketsFromPlate(string licenseplate)
+        {
+            return _vehicles.Where(v => v.Licenseplate == licenseplate).ToList();
+        }
+    }
+}
---
+++ TestBridge/TestReposetoryStorbealt.cs
@@ -0,0 +1,48 @@
+﻿using Bridge;
+using StoreBealtTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBridge
+{
+    [TestClass]
+    public sealed class TestReposetoryStorbealt
+    {
+        [TestMethod]
+        public void GetAllTickets()
+        {
+            //Arrange
+            Reposetory storRepo = new Reposetory();
+            List<Vehicle> listOfV = storRepo.GetAllTickets();
+            Vehicle v = new Car("1111111", new DateTime(2025, 07, 05), false);
+
+            //act
+            storRepo.AddTicket(v);
+            
+
+            //Assert
+            Assert.IsNotNull(listOfV);
+        }
+
+        [TestMethod]
+        public void TestAddTicket() 
+        {
+            //Arrange
+            Reposetory storRepo = new Reposetory();
+            List < Vehicle > listOfV= storRepo.GetAllTickets();
+            Vehicle v = new Car("1111111", new DateTime(2025, 07, 05), false);
+
+            //act
+            int countBEfore = listOfV.Count();
+            storRepo.AddTicket(v);
+            int countAfter = listOfV.Count();
+
+            //Assert
+            Assert.AreEqual(countBEfore+1, countAfter);
+        }
+
+    }
+}
